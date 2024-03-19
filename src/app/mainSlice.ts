ed5be0418eebef1815cd621092a0984de5f9367d
Daniil848 <ladygindaniil848@gmail.com>
2024-03-19 11:35:24 +0300

    mainSlice index added

---
+++ src/app/mainSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+export interface State {
+  loading: boolean;
+  error: boolean;
+}
+
+const initialState: State = {
+  loading: false,
+  error: false,
+};
+
+const mainSlice = createSlice({
+  name: 'mainSlice',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder;
+  },
+});
+
+// export const {} = mainSlice.actions;
+
+export default mainSlice.reducer;
