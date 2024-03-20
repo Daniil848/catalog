2ae8622c4d4c64f9df89a78917a3fa6531a7ff83
Daniil848 <ladygindaniil848@gmail.com>
2024-03-20 11:41:04 +0300

    create Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes } from 'react-router-dom';
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes></Routes>
     </div>
   );
