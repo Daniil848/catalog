38f4d7ba876ce58fa9aa41c8971778e61b56f511
Daniil848 <ladygindaniil848@gmail.com>
2024-03-20 11:49:56 +0300

    create HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import './App.css';
+import HomePage from './pages/homePage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes></Routes>
+      <Routes>
+        <Route index element={<HomePage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/homePage/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './HomePage.module.scss';
+
+const HomePage = () => {
+  return <></>;
+};
+
+export default HomePage;
