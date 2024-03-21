3d7b3fb96b8abc7e81e61410931a2e621267663b
Daniil848 <ladygindaniil848@gmail.com>
2024-03-21 12:02:58 +0300

    animate accordion

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { getCategories, getProducts } from '../../app/mainSlice';
+import { Category, getCategories, getProducts } from '../../app/mainSlice';
+import { motion, AnimatePresence } from 'framer-motion';
 import ProductCard from '../../components/productCard/ProductCard';
 import styles from './HomePage.module.scss';
+
+interface Accordion {
+  [key: string]: boolean;
+}
 
 const HomePage = () => {
   const state = useAppSelector((state) => state.mainSlice);
   const dispatch = useAppDispatch();
 
-  const [accordion, setAccordion] = useState<boolean>(false);
+  const [accordion, setAccordion] = useState<Accordion>({});
 
   useEffect(() => {
     dispatch(getCategories());
     dispatch(getProducts());
   }, []);
+
+  const handleAccordion = (category: Category) => {
+    setAccordion((prevState: any) => ({
+      ...prevState,
+      [category.id]: !prevState[category.id] || false,
+    }));
+  };
 
   return (
     <>
@@ -23,20 +35,33 @@ const HomePage = () => {
             <div className={styles.category}>
               <p
                 className={styles.categoryName}
-                onClick={() => setAccordion(!accordion)}
+                onClick={() => handleAccordion(category)}
               >
                 {category.name}
               </p>
             </div>
-            <div
-              className={accordion ? styles.products : styles.hiddenProducts}
-            >
-              {state.products
-                .filter((el) => el.categoryId == category.id)
-                .map((product) => (
-                  <ProductCard product={product} key={product.id} />
-                ))}
-            </div>
+            <AnimatePresence>
+              {accordion[category.id] && (
+                <motion.div
+                  className={styles.products}
+                  initial={{ opacity: 0, height: 0 }}
+                  animate={{
+                    opacity: 1,
+                    height: 'auto',
+                    transition: {
+                      duration: 0.5,
+                    },
+                  }}
+                  exit={{ opacity: 0, height: 0 }}
+                >
+                  {state.products
+                    .filter((el) => el.categoryId == category.id)
+                    .map((product) => (
+                      <ProductCard product={product} key={product.id} />
+                    ))}
+                </motion.div>
+              )}
+            </AnimatePresence>
           </div>
         ))}
       </div>
