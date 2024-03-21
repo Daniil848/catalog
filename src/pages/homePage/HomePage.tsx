9b1bfe3ef025412645300d9f8c54943033330dc3
Daniil848 <ladygindaniil848@gmail.com>
2024-03-21 10:51:32 +0300

    uninstall react spring

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { getCategories, getProducts } from '../../app/mainSlice';
-import styles from './HomePage.module.scss';
 import ProductCard from '../../components/productCard/ProductCard';
+import styles from './HomePage.module.scss';
 
 const HomePage = () => {
   const state = useAppSelector((state) => state.mainSlice);
