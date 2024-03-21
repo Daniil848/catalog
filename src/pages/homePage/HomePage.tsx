4a7b1fa1e9461276b4a495e82036067f1d6bdabd
Daniil848 <ladygindaniil848@gmail.com>
2024-03-21 10:31:58 +0300

    accordion

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { getCategories, getProducts } from '../../app/mainSlice';
 import styles from './HomePage.module.scss';
@@ -7,6 +7,8 @@ import ProductCard from '../../components/productCard/ProductCard';
 const HomePage = () => {
   const state = useAppSelector((state) => state.mainSlice);
   const dispatch = useAppDispatch();
+
+  const [accordion, setAccordion] = useState<boolean>(false);
 
   useEffect(() => {
     dispatch(getCategories());
@@ -18,8 +20,17 @@ const HomePage = () => {
       <div className={styles.container}>
         {state.categories.map((category) => (
           <div key={category.id}>
-            <p className={styles.categoryName}>{category.name}</p>
-            <div className={styles.products}>
+            <div className={styles.category}>
+              <p
+                className={styles.categoryName}
+                onClick={() => setAccordion(!accordion)}
+              >
+                {category.name}
+              </p>
+            </div>
+            <div
+              className={accordion ? styles.products : styles.hiddenProducts}
+            >
               {state.products
                 .filter((el) => el.categoryId == category.id)
                 .map((product) => (
