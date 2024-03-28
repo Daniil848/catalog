73b5c98eaef3c858f968d5b135fdc05b626fd0f5
Daniil848 <ladygindaniil848@gmail.com>
2024-03-28 11:31:01 +0300

    create PrintModal files

---
+++ src/components/printModal/PrintModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { usePrintModal } from './usePrintModal';
+import styles from './PrintModal.module.scss';
+
+const PrintModal = () => {
+  return <></>;
+};
+
+export default PrintModal;
---
+++ src/components/printModal/usePrintModal.ts
@@ -0,0 +1,3 @@
+export const usePrintModal = () => {
+  return;
+};
