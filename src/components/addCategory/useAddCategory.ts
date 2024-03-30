965db0a5c44c066ec8ef9ed5a56afb9fb046db55
Daniil848 <ladygindaniil848@gmail.com>
2024-03-30 15:08:25 +0300

    fix handle change category name

--- src/components/addCategory/useAddCategory.ts
+++ src/components/addCategory/useAddCategory.ts
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { addCategory, setIsAddCategory } from '../../app/mainSlice';
 import { nanoid } from 'nanoid';
+import toast from 'react-hot-toast';
 
 export const useAddCategory = () => {
   const state = useAppSelector((state) => state.mainSlice);
@@ -10,9 +11,15 @@ export const useAddCategory = () => {
   const [categoryName, setCategoryName] = useState<string>('');
 
   const handleChangeCtegory = () => {
+    if (!categoryName) {
+      toast.error('Enter the name of the category');
+      return;
+    }
     dispatch(addCategory({ id: nanoid(), name: categoryName }));
     dispatch(setIsAddCategory(false));
+    setCategoryName('');
   };
+
   return {
     state,
     dispatch,
