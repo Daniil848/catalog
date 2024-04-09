c366590ab7c68099cfc5f6e5e2ba4696323f0750
Daniil848 <ladygindaniil848@gmail.com>
2024-04-09 19:55:47 +0300

    ref type

--- src/components/printSheet/PrintSheet.tsx
+++ src/components/printSheet/PrintSheet.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/display-name */
-import React from 'react';
+import React, { ForwardedRef } from 'react';
 import { usePrintSheet } from './usePrintSheet';
 import styles from './PrintSheet.module.scss';
 
@@ -8,83 +8,85 @@ interface Props {
   comment: string;
 }
 
-const PrintSheet = React.forwardRef((props: Props, ref: any) => {
-  const { state, totalPriceInCategory } = usePrintSheet();
+const PrintSheet = React.forwardRef(
+  (props: Props, ref: ForwardedRef<HTMLTableElement>) => {
+    const { state, totalPriceInCategory } = usePrintSheet();
 
-  return (
-    <>
-      <div className={styles.sheet}>
-        <table ref={ref} className="m-[2cm]">
-          <caption className="text-start p-4 border">
-            <p>Contacts: {props.contacts}</p>
-            <p>Comment: {props.comment}</p>
-          </caption>
-          <thead>
-            <tr>
-              <th className="w-fit p-4 text-center border bg-slate-100">
-                Image
-              </th>
-              <th className="w-fit p-4 text-center border bg-slate-100">
-                Name
-              </th>
-              <th className="w-fit p-4 text-center border bg-slate-100">
-                Category
-              </th>
-              <th className="w-fit p-4 text-center border bg-slate-100">
-                Quantity
-              </th>
-              <th className="w-fit p-4 text-center border bg-slate-100">
-                Price
-              </th>
-            </tr>
-          </thead>
-          {state.categories.map((category) => (
-            <tbody key={category.id}>
-              {state.history[state.history.length - state.historyIndex]
-                .filter((el) => el.categoryId == category.id)
-                .map((product) => (
-                  <tr key={product.id}>
-                    <td className="w-full min-h-[69px] p-4 border flex items-center justify-center">
-                      {product.image && (
-                        <img
-                          src={product.image}
-                          alt=""
-                          width="70"
-                          height="70"
-                        />
-                      )}
-                    </td>
-                    <td className="w-fit p-4 text-center border">
-                      {product.title}
-                    </td>
-                    <td className="w-fit p-4 text-center border">
-                      {category.name}
-                    </td>
-                    <td className="w-fit p-4 text-center border">
-                      {product.quantity}
-                    </td>
-                    <td className="w-fit p-4 text-center border">
-                      {product.price}
-                    </td>
-                  </tr>
-                ))}
-              <tr className="border border-gray-200">
-                <td className="w-fit p-4 text-center font-semibold">
-                  {category.name} total:
-                </td>
-                <td className="w-fit p-4 text-center"></td>
-                <td className="w-fit p-4 text-center"></td>
-                <td className="w-fit p-4 text-center"></td>
-                <td className="w-fit p-4 text-center font-semibold">
-                  ${totalPriceInCategory(category.id).toFixed(2)}
-                </td>
+    return (
+      <>
+        <div className={styles.sheet}>
+          <table ref={ref} className="m-[2cm]">
+            <caption className="text-start p-4 border">
+              <p>Contacts: {props.contacts}</p>
+              <p>Comment: {props.comment}</p>
+            </caption>
+            <thead>
+              <tr>
+                <th className="w-fit p-4 text-center border bg-slate-100">
+                  Image
+                </th>
+                <th className="w-fit p-4 text-center border bg-slate-100">
+                  Name
+                </th>
+                <th className="w-fit p-4 text-center border bg-slate-100">
+                  Category
+                </th>
+                <th className="w-fit p-4 text-center border bg-slate-100">
+                  Quantity
+                </th>
+                <th className="w-fit p-4 text-center border bg-slate-100">
+                  Price
+                </th>
               </tr>
-            </tbody>
-          ))}
-        </table>
-      </div>
-    </>
-  );
-});
+            </thead>
+            {state.categories.map((category) => (
+              <tbody key={category.id}>
+                {state.history[state.history.length - state.historyIndex]
+                  .filter((el) => el.categoryId == category.id)
+                  .map((product) => (
+                    <tr key={product.id}>
+                      <td className="w-full min-h-[69px] p-4 border flex items-center justify-center">
+                        {product.image && (
+                          <img
+                            src={product.image}
+                            alt=""
+                            width="70"
+                            height="70"
+                          />
+                        )}
+                      </td>
+                      <td className="w-fit p-4 text-center border">
+                        {product.title}
+                      </td>
+                      <td className="w-fit p-4 text-center border">
+                        {category.name}
+                      </td>
+                      <td className="w-fit p-4 text-center border">
+                        {product.quantity}
+                      </td>
+                      <td className="w-fit p-4 text-center border">
+                        {product.price}
+                      </td>
+                    </tr>
+                  ))}
+                <tr className="border border-gray-200">
+                  <td className="w-fit p-4 text-center font-semibold">
+                    {category.name} total:
+                  </td>
+                  <td className="w-fit p-4 text-center"></td>
+                  <td className="w-fit p-4 text-center"></td>
+                  <td className="w-fit p-4 text-center"></td>
+                  <td className="w-fit p-4 text-center font-semibold">
+                    ${totalPriceInCategory(category.id).toFixed(2)}
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+      </>
+    );
+  },
+);
 
 export default PrintSheet;
